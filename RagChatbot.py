1de4c2db985ce03f18890f9ed1c26a0b188f2a15
Wyatt Davis <whyit11@gmail.com>
2025-09-25 01:01:30 -0500

    removing file upload element after uploading and processing file. Adding success message after upload and processing. Adding informational blurb about application.

--- RagChatbot.py
+++ RagChatbot.py
@@ -10,6 +10,7 @@ from streamlit_lottie import st_lottie
 import json
 import requests
 import pandas as pd
+import time
 
 import streamlit as st
 
@@ -25,11 +26,13 @@ retriever = vector_store.as_retriever(
 )
 
 # Set up model
-model = OllamaLLM(model="phi4:14b")
+model = OllamaLLM(model="phi4-mini")
 model.temperature = .6
 
 #Title streamlit chat window
 st.title("D&D Q&A Chatbot 🧙‍♂️")
+
+st.info("This app takes your notes from your campaign and passes relevant context from them along with your question to the LLM. It does not store your notes or chat history. Please consult provided references as the AI may hallucinate.")
 
 #Grab custom spinner animation
 with open("star-magic.json", "r",errors='ignore') as f:
@@ -38,12 +41,23 @@ with open("star-magic.json", "r",errors='ignore') as f:
 # Set up retriever in streamlit app
 st.session_state.retriever = retriever
 
-disable_loader = False
+notes_uploaded = False
 
-# Have user upload campaign notes
-note_document = st.file_uploader(
-    "Upload Campaign Notes", accept_multiple_files=False, type="csv", disabled = disable_loader
-)
+if "uploader_key" not in st.session_state:
+    st.session_state.uploader_key = 0
+
+def update_key():
+    st.session_state.uploader_key += 1
+
+note_document = None
+
+if st.session_state.uploader_key == 0:
+    placeholder = st.empty()
+    # Have user upload campaign notes
+    with placeholder.container():
+        note_document = st.file_uploader("Upload your campaign notes", type=["csv"]) #key=st.session_state.uploader_key, on_change=update_key
+else:
+    notes_uploaded = True
 
 # Create vector database from file
 if note_document is not None:
@@ -70,7 +84,17 @@ if note_document is not None:
                 )
     if vector_store != None: # To do: add error checking
         vector_store.add_documents(documents=documents, ids=ids)
-    disable_loader = True
+
+        #message_placeholder = st.empty()
+        update_key()
+        placeholder.empty()
+        success = st.success("Campaign notes uploaded and processed successfully!")
+        
+
+    notes_uploaded = True
+    
+
+
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -82,42 +106,41 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 
-placeholder = st.empty()
-
 #user_question = # Show chat input at the bottom when a question has been asked.
-user_question = st.chat_input("Ask a question about the campaign...")
-if user_question:
-    st.session_state.messages.append({"role": "user", "content": user_question,"avatar":None})
-    with st.chat_message("user"):
-        st.markdown(user_question)
-    
-    placeholder = st.empty()
-    # Display the animation initially
-    with placeholder.container():
-        st_lottie(magic_spinner, height=200, key="custom_spinner")
-
-    #with st.spinner("Consulting texts..."):
-    prompt = ChatPromptTemplate.from_messages([
-        ("system", "You are a helpful D&D adventure Q&A bot."),
-        ("user", "You are an expert in answering questions about a Dungeons and Dragons campaign described in provided documents. The provided documents describe a campaign where the main protagonists are Brocc, Evryn, and Gwendolyn(Gwen). Here are the relevant documents with a date and title from the character Brocc's perspective (sometimes in first person and sometimes in third person): {notes} \n\n Here is the question to answer. Base your answer only off of the provided documents, and no other extraneous material. Do not provide references to the documents.: {question}")
-    ])
+if notes_uploaded:
+    user_question = st.chat_input("Ask a question about the campaign...")
+    if user_question:
+        st.session_state.messages.append({"role": "user", "content": user_question,"avatar":None})
+        with st.chat_message("user"):
+            st.markdown(user_question)
         
-    notes = retriever.invoke(user_question)
+        placeholder = st.empty()
+        # Display the animation initially
+        with placeholder.container():
+            st_lottie(magic_spinner, height=200, key="custom_spinner")
 
-    chain = (
-        prompt
-        | model
-        | StrOutputParser()
-    )
-    response = chain.invoke({"question": user_question, "notes": notes})  # Pass the query and relevant note documents
+        #with st.spinner("Consulting texts..."):
+        prompt = ChatPromptTemplate.from_messages([
+            ("system", "You are a helpful D&D adventure Q&A bot."),
+            ("user", "You are an expert in answering questions about a Dungeons and Dragons campaign described in provided documents. The provided documents describe a campaign where the main protagonists are Brocc, Evryn, and Gwendolyn(Gwen). Here are the relevant documents with a date and title from the character Brocc's perspective (sometimes in first person and sometimes in third person): {notes} \n\n Here is the question to answer. Base your answer only off of the provided documents, and no other extraneous material. Do not provide references to the documents.: {question}")
+        ])
+            
+        notes = retriever.invoke(user_question)
 
-    response+="\n______________________________________________________\n"
-    response+="Note entry References(date): \n"
-    for item in notes:
-        response += "* " + item.metadata["Date"] + "\n"
-    response+="\n______________________________________________________\n"
-    st.session_state.messages.append({"role": "assistant", "content": response, "avatar":"🧙‍♂️"})
-    placeholder.empty()
-    with st.chat_message("assistant", avatar="🧙‍♂️"):
-        st.markdown(response)
+        chain = (
+            prompt
+            | model
+            | StrOutputParser()
+        )
+        response = chain.invoke({"question": user_question, "notes": notes})  # Pass the query and relevant note documents
+
+        response+="\n______________________________________________________\n"
+        response+="Note entry References(date): \n"
+        for item in notes:
+            response += "* " + item.metadata["Date"] + "\n"
+        response+="\n______________________________________________________\n"
+        st.session_state.messages.append({"role": "assistant", "content": response, "avatar":"🧙‍♂️"})
+        placeholder.empty()
+        with st.chat_message("assistant", avatar="🧙‍♂️"):
+            st.markdown(response)
   
