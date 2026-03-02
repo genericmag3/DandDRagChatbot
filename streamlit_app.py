705fd49445730b72633f8c29407a258cf364e4cc
Wyatt Davis <whyit11@gmail.com>
2026-03-02 12:13:36 -0600

    further functionalization and consolidation of variables into sessions state

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,22 @@ import numpy as np
 #import local modules
 import src.utils.CreateDatabase as CreateDatabase
 
+# Initialize session state variables for model, database upload handling, and document retriever
+if ("uploader_key" not in st.session_state) or ("reupload_key" not in st.session_state) or ("model_chosen" not in st.session_state) or ("model_temperature" not in st.session_state) or ("document_retriever" not in st.session_state):
+    st.session_state.uploader_key = 0
+    st.session_state.reupload_key = 0
+    st.session_state.model_chosen = None 
+    st.session_state.model_temperature = None
+    st.session_state.notes_uploaded = False
+    st.session_state.document_retriever = None
+
+# If the chat history or button info does not exist in session state, or if the user opts to re-upload notes, initialize chat history, button info, and button key in session state
+if ("messages" not in st.session_state) or ("buttoninfo" not in st.session_state) or ("button_key" not in st.session_state) or (st.session_state.reupload_key == True):
+    st.session_state.messages = []
+    st.session_state.buttoninfo = []
+    st.session_state.button_key = 0
+
+
 # Local Function definitions 
 
 # Load LLM
@@ -30,9 +46,9 @@ def has_subfolders(directory_path):
             return True
     return False
 
-def create_database_handler(document, databasedir, text_splitter, retriever, vector_store):
+def create_database_handler(document, databasedir, text_splitter, vector_store):
     # Start database creation
-    gen = CreateDatabase.vectorize_note_document(document, databasedir, text_splitter, retriever, vector_store)
+    gen = CreateDatabase.vectorize_note_document(document, databasedir, text_splitter, st.session_state.document_retriever, vector_store)
 
     # Grab custom spinner animation
     with open("assets/Magical_Effect_Loading.json", "r",errors='ignore') as f:
@@ -67,7 +83,7 @@ def stream_data(response):
         time.sleep(0.02)
 
 # Check for new upload
-def process_sidebar_options():
+def process_model_options():
     # Find local ollama models 
     local_models = ollama.list()
     local_model_names = []
@@ -79,12 +95,50 @@ def process_sidebar_options():
     if ((sidebar_model_select is not None) and (sidebar_model_temperature is not None)):
         st.session_state.model_chosen = load_model(sidebar_model_select)
         st.session_state.model_chosen.temperature = sidebar_model_temperature
-        
-       # model = load_model(st.session_state.model_chosen)
-       # model.temperature = st.session_state.model_temperature
     else:
         st.session_state.model_chosen = None
         st.session_state.model_temperature = None
+
+def process_journal_options(data_base_directory):
+    note_document = None
+    if has_subfolders(data_base_directory) and st.session_state.reupload_key == False:
+        st.session_state.notes_uploaded = True
+        update_key()
+        sidebar_button = st.sidebar.button('Re-Upload Notes')
+        if sidebar_button:
+            if os.path.exists(data_base_directory):
+                st.session_state.reupload_key = True
+                st.rerun()
+
+    # if the database does not exist, or the user opted to re-uplaod notes, have user upload notes and create database
+    else:
+        st.session_state.notes_uploaded = False
+        placeholder = st.empty()
+        # Have user upload campaign notes
+        with placeholder.container():
+            note_document = st.file_uploader("Upload your campaign notes")
+
+    # Init text splitter, retriever, and vector database
+    text_splitter, st.session_state.document_retriever, vector_store = CreateDatabase.create_hf_retrival_artifacts(data_base_directory)
+
+    # Check to see if user uploaded notes
+    completionmessage = None
+    # Create vector database from file if file has been uploaded by user
+    if note_document is not None:
+        #get rid of the file uploader container once file has been selected
+        placeholder.empty()
+        #Clear reupload key just in case
+        st.session_state.reupload_key = False
+        #start data upload and database creation animation
+        st.session_state.notes_uploaded = create_database_handler(note_document, databasedir, text_splitter, vector_store)
+        if(st.session_state.notes_uploaded == True):
+            completionmessage = st.empty()
+            st.success("Journal processed successfully!")
+            st.rerun()
+        else:
+            completionmessage = st.empty()
+            st.error("Failed to vectorize database. Check file existence or disk space.")
+    return completionmessage
 
 def display_message_history():
     i = 0 #  represents index of references, each index can have multiple references and there is one per bot response
@@ -103,74 +157,21 @@ def display_message_history():
 def reference_button(content):
     st.write(content)
 
-# Initialize session state variables for model and database upload handling
-if ("uploader_key" not in st.session_state) or ("reupload_key" not in st.session_state) or ("model_chosen" not in st.session_state) or ("model_temperature" not in st.session_state):
-    st.session_state.uploader_key = 0
-    st.session_state.reupload_key = 0
-    st.session_state.model_chosen = None 
-    st.session_state.model_temperature = None
-    st.session_state.notes_uploaded = False
-
-# Ff the chat history or button info does not exist in session state, or if the user opts to re-upload notes, initialize chat history, button info, and button key in session state
-if ("messages" not in st.session_state) or ("buttoninfo" not in st.session_state) or ("button_key" not in st.session_state) or (st.session_state.reupload_key == True):
-    st.session_state.messages = []
-    st.session_state.buttoninfo = []
-    st.session_state.button_key = 0
-
 # Initialize Streamlit UI 
 
 st.title("TTRPG Journal Q&A Chatbot 🧙‍♂️")
 
 st.info("This app takes your notes from your TTRPG campaign and passes your question along with relevant context from your notes to the local LLM. It does not permenantly store your notes or chat history or use them to train any model. Please consult provided references as the AI may hallucinate.")
 
-process_sidebar_options() 
-
-note_document = None
-
 # Database directory location
 databasedir = "database/chrome_langchain_db"
 
-# if the database already exists, skip the upload and allow for re-upload sidebar option 
-if has_subfolders(databasedir) and st.session_state.reupload_key == False:
-    st.session_state.notes_uploaded = True
-    update_key()
-    sidebar_button = st.sidebar.button('Re-Upload Notes')
-    if sidebar_button:
-        if os.path.exists(databasedir):
-            st.session_state.reupload_key = True
-            st.rerun()
+process_model_options() 
 
-# if the database does not exist, or the user opted to re-uplaod notes, have user upload notes and create database
-#elif st.session_state.uploader_key == 0 or st.session_state.reupload_key == True:
-elif st.session_state.reupload_key == True:
-    st.session_state.notes_uploaded = False
-    placeholder = st.empty()
-    # Have user upload campaign notes
-    with placeholder.container():
-        note_document = st.file_uploader("Upload your campaign notes")
+# process journal options upon re-run, fetch retriver for journal and upload completion message
+completionmessage = process_journal_options(databasedir)
 
-# Init text splitter, retriever, and vector database
-text_splitter, retriever, vector_store = CreateDatabase.create_hf_retrival_artifacts(databasedir)
-
-# Check to see if user uploaded notes
-
-completionmessage = None
-# Create vector database from file if file has been uploaded by user
-if note_document is not None:
-    #get rid of the file uploader container once file has been selected
-    placeholder.empty()
-    #Clear reupload key just in case
-    st.session_state.reupload_key = False
-    #start data upload and database creation animation
-    st.session_state.notes_uploaded = create_database_handler(note_document, databasedir, text_splitter, retriever, vector_store)
-    if(st.session_state.notes_uploaded == True):
-        completionmessage = st.empty()
-        st.success("Journal processed successfully!")
-        st.rerun()
-    else:
-        completionmessage = st.empty()
-        st.error("Failed to vectorize database. Check file existence or disk space.")
-    
+# Display chat history upon re-run
 display_message_history()
 
 #Chat logic, only runs if notes have been uploaded, a model has been chosen, and a model temperature has been set
@@ -202,12 +203,14 @@ if st.session_state.notes_uploaded and (st.session_state.model_chosen is not Non
             "{notes} \n\n Here is the question to answer. Base your answer only off of the provided documents, and no other extraneous material. "
             "Do not provide references to the documents.: {question}")
         ])
-        notes = retriever.invoke(user_question)
+        notes = st.session_state.document_retriever.invoke(user_question)
         chain = (
             prompt
             | st.session_state.model_chosen
             | StrOutputParser()
         )
+
+        # Pass user query plus relevant notes to the model and get response if relevant notes are found
         if len(notes) > 0:
             response = chain.invoke({"question": user_question, "notes": notes})  # Pass the query and relevant note documents
             placeholder.empty()
@@ -230,6 +233,7 @@ if st.session_state.notes_uploaded and (st.session_state.model_chosen is not Non
 
                     # Add reference button information for the response to the session state            
                     st.session_state.buttoninfo.append(tempbuttoninfo) 
+
         # Save canned response to chat history if no references
         else:
             placeholder.empty()
